3dc90232916df185240de02fd13cb7ee283652bd
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-06-13 15:49:24 +0300

    design basic operation logic

--- js.js
+++ js.js
@@ -2,4 +2,45 @@ const add = (num1, num2) => num1 + num2;
 const sub = (num1, num2) => num1 - num2; 
 const multiply = (num1, num2) => num1 * num2; 
 const divide = (num1, num2) => num1 / num2;
-console.log(divide(10, 2));
+const evaluate = () => {
+    display.textContent = operate(operation, nums[0], nums[1]);
+}
+
+const operate = (type, num1, num2) => {
+    let result ; 
+    type === 1 ? result = add(num1, num2) :
+    type === 2 ? result = sub(num1, num2) : 
+    type === 3 ? result = multiply(num1, num2) : 
+    result = divide(num1, num2);   
+    return result; 
+}
+
+
+const operations = document.querySelectorAll(".operation");
+const display = document.querySelector("#display");  
+let num1, num2 ;
+const nums = new Array(2);
+let clicks = 0; 
+let operation;
+
+operations.forEach(operation => {
+    operation.addEventListener("click", () => {
+        if(clicks != 2) {
+            operation.textContent == "+" ? operation = 1 :
+            operation.textContent == "-" ? operation = 2 : 
+            operation.textContent == "x" ? operation = 3 : 
+            operation = 4;
+
+            nums[clicks] = Number.parseInt(display.textContent);
+            display.textContent = ""; 
+            clicks++;
+        }
+        else {
+            evaluate();
+        }
+    });
+});
+
+const equals = document.querySelector("#equals"); 
+
+equals.addEventListener("click", evaluate);
