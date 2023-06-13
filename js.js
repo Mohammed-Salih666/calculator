a2f0613ec5a16536a33ad5f32bf8669c4bf4f15c
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-06-13 15:15:22 +0300

    Add basic operation functions (add, subtract, multiply, divide).

---
+++ js.js
@@ -0,0 +1,5 @@
+const add = (num1, num2) => num1 + num2; 
+const sub = (num1, num2) => num1 - num2; 
+const multiply = (num1, num2) => num1 * num2; 
+const divide = (num1, num2) => num1 / num2;
+console.log(divide(10, 2));
