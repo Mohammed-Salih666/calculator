b44181f77ce6a388d2b72878257bbdcc1d98648e
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-06-15 15:52:03 +0300

    Change the logic to solve successive operations isssue
    Calculations like (12 + 7 - 5 * 3) were not working as desired. Problem solved by introducing currentOperation and nextOperation variables, and got rid of the "nums" array for more simplicity.

--- js.js
+++ js.js
@@ -2,10 +2,16 @@ const add = (num1, num2) => num1 + num2;
 const sub = (num1, num2) => num1 - num2; 
 const multiply = (num1, num2) => num1 * num2; 
 const divide = (num1, num2) => num1 / num2;
+
+let evaluated = false; 
 const evaluate = () => {
-    display.value = operate(operationType, nums[0], nums[1]);
-    console.log(operationType + "     " + nums[0] + "   " + nums[1]);
-    nums[0] = Number.parseInt(display.value);
+
+    display.value = operate(currentOperation, num1, num2);
+    num1 = Number.parseInt(display.value); 
+    console.log(currentOperation + "    " + num1 + "   " + num2 ); 
+    currentOperation = nextOperation;
+    nextOperation = null;
+    evaluated = true; 
 
 }
 
@@ -17,9 +23,8 @@ const operate = (type, num1, num2) => {
     result = divide(num1, num2);   
     return result; 
 }
+
 const display = document.querySelector("#display");  
-let num1, num2 ;
-const nums = new Array(2);
 
 const numButtons = document.querySelectorAll(".number");
 
@@ -27,10 +32,11 @@ numButtons.forEach(number => {
     number.addEventListener("click", () => {
         display.value += number.textContent;
 
-        if(clicked){
-            display.value = number.textContent;
-            clicked = false;
-        }
+     if(evaluated){
+        display.value = number.textContent; 
+        evaluated = false;
+     }
+
     });
 });
 
@@ -40,32 +46,48 @@ let clicks = 0;
 let operationType;
 let clicked = false; 
 
-operations.forEach(operation => {
-    operation.addEventListener("click", () => {
-        if(clicks !== 2 && !clicked) {
-            operation.textContent == "+" ? operationType = 1 :
-            operation.textContent == "-" ? operationType = 2 : 
-            operation.textContent == "x" ? operationType = 3 : 
-            operationType = 4;
-
-            console.log(operationType);
-
-            nums[clicks] = Number.parseInt(display.value);
-            display.value = ""; 
-            clicked = true; 
-            clicks++;
-        }
-        if(clicks === 2) {
-            evaluate();
-            clicks--;
-        }
-    });
-});
-
 const equals = document.querySelector("#equals"); 
 
 equals.addEventListener("click", () => {
-    nums[1] = Number.parseInt(display.value); 
+    // nums[1] = Number.parseInt(display.value); 
+    // evaluate();
+    // clicks--;
+    num2 = Number.parseInt(display.value); 
     evaluate();
-    clicks--;
+    currentOperation = null; 
 });
+
+let currentOperation = null;
+let nextOperation = null;
+let num1 = null;
+let num2 = null;
+
+operations.forEach(operation => {
+
+    operation.addEventListener("click", () => {
+
+        const op = operation.textContent === "+" ? 1
+        : operation.textContent === "-" ? 2 
+        : operation.textContent === "x" ? 3
+        : 4; 
+
+        if(num1 === null && currentOperation === null) {
+            num1 = Number.parseInt(display.value); 
+            currentOperation = op;
+            display.value = "";
+        }
+        else if (nextOperation === null){
+            num2 = Number.parseInt(display.value);
+            nextOperation = op; 
+            // display.value =" ";
+            evaluate();
+        }
+        else if(currentOperation !== null) {
+            num2 = Number.parseInt(display.value);
+            evaluate();
+        }
+        
+
+    });
+
+} );
